36b5faee76eb57df8c5908d21006b4418923cc91
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-09 11:22:38 +0200

    initial setup

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/AppStore-API', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+
+  if (!res.headersSent) {
+    res.status(500).json({ code: 'unexpected' });
+  }
+});
+
+module.exports = app;
